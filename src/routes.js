45a3d869d5ebf36b01e3dd2c589e44fbc7fc4ea9
Emerson Pereira <emerson.pereira@compasso.com.br>
2020-03-22 01:13:44 -0300

    Cadastra usuario validando por email

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 
+import UserController from './app/controllers/UserController';
+
 const routes = new Router();
 
 routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello World!' });
+    return res.json({ message: 'Aplicação Rodando perfeitamente!' });
 });
+
+routes.post('/users', UserController.store);
 
 export default routes;
